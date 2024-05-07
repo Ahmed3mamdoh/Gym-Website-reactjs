ae6b9aaa99b72e01842a6cc8076b562646882952
Ahmed3mamdoh <ahmed3mmdh@gmail.com>
2024-05-07 03:30:47 +0300

    finishing

--- src/components/about/HeroSection.jsx
+++ src/components/about/HeroSection.jsx
@@ -6,9 +6,9 @@ const HeroSection = () => {
       className={`${styles["hero-section"]} margin-sections main-background`}
       data-aos="zoom-out-down">
       <h1 className="main-heading">
-        Start with us the body and
+        Start with us now for clean body and
         <br />
-        mind cleansing
+        mind !!
       </h1>
     </div>
   );
